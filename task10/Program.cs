5c6116770e6217c23de01e03a48d77ee5aa0f684
s.bond <xt-2000@mail.ru>
2022-11-18 03:31:46 +0300

    добавлен возвращаем task10

--- task10/Program.cs
+++ task10/Program.cs
@@ -1,1 +1,8 @@
-﻿//Н
+﻿// Напишите программу, которая прнимает на вход трехзначное число и на входе показывает вторую цифру этого числа.
+// 456 -> 5
+
+
+int rnd = new Random().Next(-999,1000);
+Console.WriteLine(rnd); 
+int res = Math.Abs((rnd / 10)%10);
+Console.WriteLine(res); 
