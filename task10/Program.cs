9fd9de2e41f4a8b1519ef7e5681dbe57295037b5
bond40rus <116297875+bond40rus@users.noreply.github.com>
2022-11-18 03:20:01 +0300

    Update Program.cs

--- task10/Program.cs
+++ task10/Program.cs
@@ -1,2 +1,8 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+// Напишите программу, которая прнимает на вход трехзначное число и на входе показывает вторую цифру этого числа.
+// 456 -> 5
+
+
+int rnd = new Random().Next(-999,1000);
+Console.WriteLine(rnd); 
+int res = Math.Abs((rnd / 10)%10);
+Console.WriteLine(res); 
