1570a531f0d0d5ac2e9b4f084996848192be9ec8
bond40rus <116297875+bond40rus@users.noreply.github.com>
2022-11-18 03:03:57 +0300

    Delete task10 directory

--- task10/Program.cs
+++
@@ -1,8 +0,0 @@
-﻿// Напишите программу, которая прнимает на вход трехзначное число и на входе показывает вторую цифру этого числа.
-// 456 -> 5
-
-
-int rnd = new Random().Next(-999,1000);
-Console.WriteLine(rnd); 
-int res = Math.Abs((rnd / 10)%10);
-Console.WriteLine(res); 
