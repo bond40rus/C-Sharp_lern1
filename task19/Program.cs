9b6764d46fd0e149f4970359f1d151e5057beab6
s.bond <xt-2000@mail.ru>
2022-11-20 22:54:13 +0300

    домашка

--- task19/Program.cs
+++ task19/Program.cs
@@ -5,25 +5,31 @@
 
 // Решил усложнить  задачку и теперь можно ввести любое число кроме числа с минусом можно было проверку написать на положительное число
 // Хотел бы узнать ваше мнение, нормальный ли код?
-Console.WriteLine("Введите число");
-long num = Convert.ToInt64(Console.ReadLine());
-string NumString = Convert.ToString(num);
-int min = 0;
-int max = NumString.Length-1;
-string res = "Одна цифра";
-while (min != max && (min < NumString.Length-1 || max > 0 )) // доп. проверка что бы не выходил за массив
+string IsPalindrom()
 {
-    //Console.WriteLine($"index = {min} {max}  number = {NumString[min]} {NumString[max]}"); // для проверки ;)
-    if (NumString[min] == NumString[max])
+    Console.WriteLine("Введите число");
+    long num = Convert.ToInt64(Console.ReadLine());
+    string NumString = Convert.ToString(num);
+    int min = 0;
+    int max = NumString.Length-1;
+    string res = "Одна цифра";
+    while (min != max && (min < NumString.Length-1 || max > 0 )) // доп. проверка что бы не выходил за массив
     {
-        res = "Палиндром";
-        min++;
-        max--;        
+        //Console.WriteLine($"index = {min} {max}  number = {NumString[min]} {NumString[max]}"); // для проверки ;)
+        if (NumString[min] == NumString[max])
+        {
+            res = "Палиндром";
+            min++;
+            max--;        
+        }
+        else
+        {
+            res = "Не палиндром";
+            break;
+        }
     }
-    else
-    {
-        res = "Не палиндром";
-        break;
-    }
+    return res;
 }
-Console.WriteLine($"{res}"); 
+string result = IsPalindrom();
+Console.WriteLine($"{result}"); 
+
--- task21/Program.cs
+++ task21/Program.cs
@@ -1,17 +1,22 @@
-﻿//Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 2D пространстве.
+﻿//Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
+//A (3,6,8); B (2,1,-7), -> 15.84
+//A (7,-5, 0); B (1,-1,9) -> 11.53
+//D = sqrt (Bx-Ax)^2+(By-Ay)^2+(Bz-Az)^2.
 
-//A (3,6); B (2,1) -> 5,09 
-//A (7,-5); B (1,-1) -> 7,21
-
-Console.WriteLine("Введите координату X точки А: ");
+Console.WriteLine("Введите координату x точки А: ");
 int Ax = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("Введите координату Y точки А: ");
+Console.WriteLine("Введите координату y точки А: ");
 int Ay = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите координату z точки А: ");
+int Az = Convert.ToInt32(Console.ReadLine());
 
-Console.WriteLine("Введите координату X точки B: ");
+
+Console.WriteLine("Введите координату x точки B: ");
 int Bx = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("Введите координату Y точки B: ");
+Console.WriteLine("Введите координату y точки B: ");
 int By = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите координату z точки B: ");
+int Bz = Convert.ToInt32(Console.ReadLine());
 
-double distance = Math.Round(Math.Sqrt(Math.Pow(Bx - Ax, 2) + Math.Pow(By - Ay,2)), 2);
+double distance = Math.Round(Math.Sqrt(Math.Pow(Bx - Ax, 2) + Math.Pow(By - Ay, 2) + Math.Pow(Bz - Az,2)), 2);
 Console.WriteLine(distance);
